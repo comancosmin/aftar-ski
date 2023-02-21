040a57f2e008a2ee8adb49ebcbe1ee6d3490af91
Cosmin <xcosmin53@gmail.com>
2023-02-21 19:45:54 +0200

    color update 2

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-fuchsia-500">
+      <body className="bg-custom-black">
         <Main />
         <NextScript />
       </body>
